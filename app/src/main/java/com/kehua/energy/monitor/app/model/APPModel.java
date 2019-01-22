c87608eef79ed44d13dc1647f2ff0054c9f8a74c
linyixian <1033813650@qq.com>
2019-01-22 16:17:20 +0800

    httpmodel

--- app/src/main/java/com/kehua/energy/monitor/app/model/APPModel.java
+++ app/src/main/java/com/kehua/energy/monitor/app/model/APPModel.java
@@ -4,6 +4,7 @@ import javax.inject.Inject;
 
 import me.walten.fastgo.base.mvp.IModel;
 
+import com.kehua.energy.monitor.app.model.http.HttpModel;
 import com.kehua.energy.monitor.app.model.local.LocalModel;
 import com.kehua.energy.monitor.app.model.remote.RemoteModel;
 
@@ -12,10 +13,13 @@ public class APPModel implements IModel {
     private RemoteModel remoteModel;
     private LocalModel localModel;
 
+    private HttpModel httpModel;
+
     @Inject
-    public APPModel(RemoteModel remoteModel, LocalModel localModel) {
+    public APPModel(RemoteModel remoteModel, LocalModel localModel, HttpModel httpModel) {
         this.remoteModel = remoteModel;
         this.localModel = localModel;
+        this.httpModel = httpModel;
     }
 
     public RemoteModel getRemoteModel() {
@@ -26,12 +30,18 @@ public class APPModel implements IModel {
         return localModel;
     }
 
+    public HttpModel getHttpModel() {
+        return httpModel;
+    }
+
     @Override
     public void destroy() {
         if (remoteModel != null)
             remoteModel.destroy();
         if (localModel != null)
             localModel.destroy();
+        if (httpModel != null)
+            httpModel.destroy();
     }
 
 
